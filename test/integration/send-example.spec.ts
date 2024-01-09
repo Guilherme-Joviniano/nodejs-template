0371791432db193396e676fcf2fa89245cf4e34d
Guilherme-Joviniano <guilherme.sousa@pagtel.com.br>
2024-01-09 10:42:29 -0300

    chore: (test) add client dependency for socket connection #13

--- test/integration/send-example.spec.ts
+++ test/integration/send-example.spec.ts
@@ -1,6 +1,7 @@
 // TODO: validate websocket connection and
 
 import { application } from '@/main/application';
+import {} from 'socket.io-client';
 
 describe('example-event Test Suite', () => {
   beforeAll(async () => {
@@ -10,4 +11,5 @@ describe('example-event Test Suite', () => {
   afterAll(async () => {
     application.close();
   });
+  it('should receive hello world message on connect to event', () => {});
 });
