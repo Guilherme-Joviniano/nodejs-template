1726409a5607f9a54dd6644f5f555e5d792d045e
Guilherme-Joviniano <guilherme.sousa@pagtel.com.br>
2024-01-09 11:10:44 -0300

    test: add client connection to server at test suite #13

--- test/integration/send-example.spec.ts
+++ test/integration/send-example.spec.ts
@@ -1,15 +1,23 @@
-// TODO: validate websocket connection and
+import { io as ioc, Socket as ClientSocket } from 'socket.io-client';
 
 import { application } from '@/main/application';
-import {} from 'socket.io-client';
+import { SERVER } from '@/util';
 
 describe('example-event Test Suite', () => {
+  let client: ClientSocket;
+
   beforeAll(async () => {
     application.setBaseUrl('/api/v1');
     application.setWebSocketServerOptions();
+    client = ioc(`http://localhost:${SERVER.PORT}`);
   });
   afterAll(async () => {
     application.close();
   });
-  it('should receive hello world message on connect to event', () => {});
+  it('should receive hello world message on connect to event', (done) => {
+    client.on('connect', () => {
+      expect(true).toBe(true);
+      done();
+    });
+  });
 });
